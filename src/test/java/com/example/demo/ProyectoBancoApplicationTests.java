98e4ce16a800fc5ba637df3044e7cfdfd8a41a33
Jafet <jafet12345@hotmail.com>
2020-05-28 15:43:21 -0500

    First commit

---
+++ src/test/java/com/example/demo/ProyectoBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
